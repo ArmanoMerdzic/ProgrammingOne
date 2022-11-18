6d9908fb0c233b827750f8c7e0c6b7d3cce1e749
Armano Merdžić <51056924+ArmanoMerdzic@users.noreply.github.com>
2022-11-18 13:01:55 +0100

    Create Task6.c

---
+++ Exercise/Task6.c
@@ -0,0 +1,19 @@
+ /* Write a program that prints all even numbers in the interval [a,b] */
+
+#include <stdio.h>
+
+int main() {
+  
+	int a, b;
+  scanf("%d %d", &a, &b);
+  
+	for (int i = a; i <= b; i++) 
+	{
+		if(i % 2 == 0) 
+		{
+		  printf("%d ", i);
+		}
+	}
+  
+	return 0;
+}
