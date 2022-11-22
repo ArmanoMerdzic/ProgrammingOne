2df884a5b7d262536f7ecc945310fd92e4ad9122
Armano Merdžić <51056924+ArmanoMerdzic@users.noreply.github.com>
2022-11-22 22:36:46 +0100

    Create Task16.c

---
+++ Exercise/Task16.c
@@ -0,0 +1,31 @@
+/* Find the first number smaller than the one entered, whose sum of the factorial of each digit is equal to that number and print the individual sums. */
+
+#include <stdio.h>
+
+int main() {
+  int n;
+  do {
+    scanf("%d", &n);
+  } while (n <= 0);
+  for (int i = n - 1; i >= 0; i--) {
+    int num = i;
+    int sum = 0;
+    while (num) {
+      int digit = num % 10;
+      int tmp = 1;
+      for (int j = digit; j > 1; j--) {
+        tmp *= j;
+      }
+      printf("%d+", tmp);
+      sum += tmp;
+      num /= 10;
+      printf("%d", tmp);
+    }
+    printf("\n");
+    if (sum == i) {
+      printf("%d", i);
+      return 0;
+    }
+  }
+  return 0;
+}
