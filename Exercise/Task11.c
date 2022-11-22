33ef082544fe57513870ff81f65856bef6cfa76a
Armano Merdžić <51056924+ArmanoMerdzic@users.noreply.github.com>
2022-11-22 22:26:38 +0100

    Create Task11.c

---
+++ Exercise/Task11.c
@@ -0,0 +1,30 @@
+/* Program that calculates the sum based on the following example: 2234 = pow(2,1) + pow(2,2) + pow(3,3) + pow(4,4) */
+
+#include <stdio.h>
+
+
+int main() {
+   int b, power = 1;
+   do {
+      scanf("%d", & b);
+   }
+   while (b <= 0);
+   
+   int sum = 0;
+   
+   while (b) 
+      {
+      int digit = b % 10;
+      int tmp = 1;
+        for (int i = 1; i <= power; i++) 
+        {
+           tmp *= digit;
+        }
+      sum += tmp;
+      power++;
+      b = b / 10;
+   }
+   printf("%d", sum);
+  
+   return 0;
+}
